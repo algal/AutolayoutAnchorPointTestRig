c27f6c6fbfb6bbaf491be9c7c6d05ce9634df227
Alexis Gallagher <alexis@alexisgallagher.com>
2012-11-08 11:34:27 +0000

    rm dead code

--- LayoutTester/InstrumentedView.m
+++ LayoutTester/InstrumentedView.m
@@ -63,40 +63,6 @@ void AddSpotToAnchorPointOfView(UIView * view) {
   return;
 }
 
-void AddSpotToAnchorPointOfView2(UIView * view) {
-  UIColor * const color = [UIColor redColor];
-  CGFloat const crossHairDimension = 7.0f;
-  
-  
-  CALayer * layer = view.layer;
-  
-  // draw a cross-hairs centered on the anchorpoint
-  CGSize layerSize = layer.bounds.size;
-  CGPoint anchorPointInLayerCoords = CGPointMake(layer.anchorPoint.x * layerSize.width,
-                                                 layer.anchorPoint.x * layerSize.height);
-  
-  UIGraphicsBeginImageContextWithOptions(layerSize, NO, 0);
-  CGContextRef c = UIGraphicsGetCurrentContext();
-  CGContextSetStrokeColorWithColor(c, [color CGColor]);
-  
-  CGContextMoveToPoint(c, anchorPointInLayerCoords.x - (crossHairDimension/2.0f), anchorPointInLayerCoords.y);
-  CGContextAddLineToPoint(c, anchorPointInLayerCoords.x + (crossHairDimension/2.0f), anchorPointInLayerCoords.y);
-  CGContextStrokePath(c);
-  CGContextMoveToPoint(c,    anchorPointInLayerCoords.x,  anchorPointInLayerCoords.y - (crossHairDimension/2.0f));
-  CGContextAddLineToPoint(c, anchorPointInLayerCoords.x , anchorPointInLayerCoords.y + (crossHairDimension/2.0f));
-  CGContextStrokePath(c);
-  
-  UIImage * overlayImage = UIGraphicsGetImageFromCurrentImageContext();
-  UIGraphicsEndImageContext();
-  
-  
-  // add it to a spotlayer
-  CALayer * spotLayer = [CALayer layer];
-  spotLayer.frame = layer.bounds;
-  spotLayer.contents = (id)[overlayImage CGImage];
-  [layer addSublayer:spotLayer];
-}
-
 @implementation InstrumentedView
 
 - (id)initWithFrame:(CGRect)frame
