bbb2dfb38bcbd8e83554747c5e06c4b8138145a4
Alexis Gallagher <alexis@alexisgallagher.com>
2012-11-08 09:07:02 +0000

    Initial Commit

---
+++ LayoutTester/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LayoutTester
+//
+//  Created by Alexis Gallagher on 2012-11-08.
+//  Copyright (c) 2012 Foxtrot Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LayoutTester/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LayoutTester
+//
+//  Created by Alexis Gallagher on 2012-11-08.
+//  Copyright (c) 2012 Foxtrot Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
